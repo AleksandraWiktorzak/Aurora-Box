5166bc1007e3e28ae4ed1d2097fe8261348cdedc
AleksandraWiktorzak <44510430+AleksandraWiktorzak@users.noreply.github.com>
2019-07-09 18:13:59 +0200

    Nawigacja w trakcie
    Dodany pasek z kategoriami, część hover w trakcie. Do zrobienia RWD i wercja menu na mobile.

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,9 @@
-// focus na input
 var searchBox = document.getElementById('search-box');
 searchBox.onclick = function(){
     console.log('hello');
     searchBox.style.borderBottom ="1px solid #191919";
 }
-// otwieranie koszyka
+// otwieranie koszyka podwójnym kliknięciem dedykowane dla urządzeń dotykowych
 var basket = document.getElementById('basket');
 var content = document.getElementById('basket-content');
 basket.ondblclick = function(){
